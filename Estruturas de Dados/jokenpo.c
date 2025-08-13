cb5103720a9716a4d57cc0096fbbffeafcb64d2c
Farraelll <rfmms42@gmail.com>
2025-08-13 09:09:40 -0300

    Adição do curso de estrutura de dados e informações básicas adicionadas no README

---
+++ Estruturas de Dados/jokenpo.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    char primeiro;
+    char segundo;
+
+    printf("Primeiro jogador, escolha pedra(r), papel(p) ou tesoura(t):\n");
+    scanf("%c", &primeiro);
+    printf("\nSegundo jogador, escolha pedra(r), papel(p) ou tesoura(t):\n");
+    scanf(" %c", &segundo);
+
+    if (primeiro == segundo) {
+        printf("Empate");
+    }
+    else if (primeiro == 'r' &&  segundo == 'p')
+    {
+        printf("Segundo jogador ganho krl porra :)");
+    }
+    else if (primeiro == 'p' &&  segundo == 't')
+    {
+        printf("Segundo jogador ganho krl porra :)");
+    }
+    else if (primeiro == 't' &&  segundo == 'r')
+    {
+        printf("Segundo jogador ganho krl porra :)");
+    }
+    else
+    {
+        printf("Primeiro jogador ganhou:D");
+    }
+    
+    printf("\nJogador 1: %c\nJogador 2: %c\n", primeiro, segundo);
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # curso-bsi
+
+Tive a ideia de salvar todo código escrito durante as aulas do Bacharel em Sistemas de Informação em um repositório do GitHub.
+Começando a partir do terceiro período, tudo que eu criar que tenha relação com o curso vai ficar aqui, a não ser que seja um projeto muito grande, nesse caso ele vai ficar em um outro repositório separado, já que normalmente esses projetos são feitos em grupo.
