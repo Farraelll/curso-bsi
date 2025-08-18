18d4cb8d46bf7b1acf782a5a3131d91d44dfcecd
Farraelll <rfmms42@gmail.com>
2025-08-18 09:52:14 -0300

    Aleatoriedade de verdade na máquina

--- Estruturas de Dados/jokenpoSingle.c
+++ Estruturas de Dados/jokenpoSingle.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 char escolheJogada() {
     int escolha = 1 + rand() % 4;
@@ -19,6 +20,7 @@ char escolheJogada() {
 int main() {
     char humano, maquina;
     int vitorias = 0, empates = 0;
+    srand(time(NULL));
     int rodadas = 1 + rand() % 10;
 
     for (int c = 0; c < rodadas; c++) {
@@ -46,6 +48,8 @@ int main() {
             vitorias++;
         };
     };
+    printf("\nRodadas: %i\n", rodadas);
+    system("clear");
     printf("\nHumano: %d\n", vitorias);
     printf("Maquina: %d\n", (rodadas - vitorias) - empates);
     printf("Empates: %d\n", empates);
